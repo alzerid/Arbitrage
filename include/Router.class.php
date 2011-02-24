b5cf8f8ffa1eabdfe7533a6b3baa37a7bcf1fb61
Eric Janik <eric@t3mediainc.com>
2011-02-24 16:47:15 -0600

    updated router logic to load Ajax controller files

--- include/Router.class.php
+++ include/Router.class.php
@@ -15,40 +15,20 @@ class Router
 		$controller_path = $_conf['approotpath'] . "app/controllers/$controller.php";
 		require_once($controller_path);
 
+		//check to see if we are calling ajax
+		if(isset($_GET['_ajax']))
+		{
+			//require the base controller
+			$controller_path = $_conf['approotpath'] . "app/ajax/$controller.php";
+			require_once($controller_path);
+			$controller     .= "Ajax";
+		}
+
 		$controller_name = ucfirst($controller) . "Controller";
 		$controller = new $controller_name($controller, $view);
 
 		return $controller;
 	}
 	
-  static public function getAPI()
-  {
-    global $_conf;
-
-    $route  = $_GET['_route_api'];
-    $api    = dirname($route);
-    $method = basename($route);
-
-    //Find file to route to
-    $file = $_conf['fsapipath'] . "$api/api.php";
-
-    //Api does not exists, return
-    if(!file_exists($file))
-      return NULL;
-
-    //include the api file
-    require_once($file);
-
-    //Construct the api object
-    $api = str_replace("/", " ", $api);
-    $api = ucwords($api);
-    $api = str_replace(" ", "_", $api);
-    $api = $api . "API";
-
-    //Construct object
-    $api = new $api($method);
-
-    return $api;
-  }
 }
 ?>
