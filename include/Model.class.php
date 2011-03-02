8730f919e77e11a0892214893a4857e0b785d632
Eric Janik <eric@t3mediainc.com>
2011-03-02 16:39:57 -0600

    Modified model classes to be more like a real model class

--- include/Model.class.php
+++ include/Model.class.php
@@ -1,28 +1,47 @@
 <?
+define("MODEL_INSERT", 0x00);
+define("MODEL_UPDATE", 0x01);
+
 abstract class Model
 {
 	protected $_variables;
+	protected $_originals;
 
-	public function __construct($data, $pre='')
+	protected $_db;
+	protected $_table;
+	protected $_class;
+
+	public function __construct($_db, $_table, $_class, $data, $pre='')
 	{
-		foreach($data as $k=>$v)
+		//Set db and table
+		$this->_db    = $_db;
+		$this->_table = $_table;
+		$this->_class = $_class;
+
+		$this->_variables = array();
+		$this->_originals = array();
+
+		if(count($data))
 		{
-			if($pre != '')
+			foreach($data as $k=>$v)
 			{
-				$spos = strpos($k, $pre);
-				if($spos === false)
-					continue;
+				if($pre != '')
+				{
+					$spos = strpos($k, $pre);
+					if($spos === false)
+						continue;
 
-				$key = substr($k, strlen($pre));
+					$key = substr($k, strlen($pre));
+				}
+				else
+					$key = $k;
+
+				$this->_originals[$key] = $v;
 			}
-			else
-				$key = $k;
 
-			$this->_variables[$key] = $v;
+			//Normalize any variables
+			$this->normalize();
 		}
-
-		//Normalize any variables
-		$this->normalize();
 	}
 
 	static public function requireModel($model)
@@ -32,14 +51,34 @@ abstract class Model
 		require_once($file);
 	}
 
-	abstract public function fromForm($model);
-	abstract public function toForm();
+	static public function model($class)
+	{
+		return new $class(array());
+	}
+
+	abstract public function save();
+	abstract public function update();
+
+	//Bulk/Single operations
+	abstract public function findAll($condition = array());
+	abstract public function findOne($condition = array());
+	abstract public function remove($condition = array());
+
+
 	abstract protected function normalize();
-	abstract protected function toQuery();
 	
 	public function toArray()
 	{
-		return $this->_variables;
+		$vars = array();
+		foreach($this->_originals as $key=>$value)
+		{
+			if(isset($this->_variables[$key]))
+				$vars[$key] = $this->_variables[$key];
+			else
+				$vars[$key] = $value;
+		}
+
+		return $vars;
 	}
 
 	public function __get($name)
@@ -47,13 +86,26 @@ abstract class Model
 		if(isset($this->_variables[$name]))
 			return $this->_variables[$name];
 
+		if(isset($this->_originals[$name]))
+			return $this->_originals[$name];
+
 		return NULL;
 	}
 
-	protected function _updateEntry($key, &$data)
+	public function __set($name, $value)
+	{
+		$this->_variables[$name] = $value;
+	}
+
+	public function __isset($name)
+	{
+		return ($this->$name !== NULL);
+	}
+
+	/*protected function _updateEntry($key, &$data)
 	{
 		if(array_key_exists($key, $this->_variables))
 			$data[$k] = $this->$key;
-	}
+	}*/
 }
 ?>
--- include/MongoModel.class.php
+++ include/MongoModel.class.php
@@ -3,64 +3,181 @@ class MongoModel extends Model
 {
 	public function normalize()
 	{
-		if(array_key_exists('_id', $this->_variables))
-			$this->_variables['_id'] = new MongoId($this->_variables['_id']);
+		if(array_key_exists('_id', $this->_originals))
+			$this->_originals['_id'] = new MongoId($this->_originals['_id']);
 
 		//Type casting
 		$this->_typeCastValues();
 	}
 
-	public function fromForm($a)
+	public function findAll($condition = array())
 	{
+		$mongo = MongoFactory::getInstance();
+		$db    = $this->_db;
+		$table = $this->_table;
+		$class = $this->_class;
+
+		//Find entry
+		$rows = $mongo->$db->$table->find($condition);
+		$ret  = array();
+		foreach($rows as $row)
+			$ret[] = new $class($row);
+
+		return $ret;
 	}
 
-	public function toForm()
+	public function findOne($condition = array())
 	{
+		$mongo = MongoFactory::getInstance();
+		$db    = $this->_db;
+		$table = $this->_table;
+		$class = $this->_class;
+
+		//Find entry
+		$ret = $mongo->$db->$table->findOne($condition);
+
+		return new $class($ret);
 	}
 
-	private function _getDotNotationValue($property)
+	public function remove($condition = array())
 	{
-		$ex = explode('.', $property);
+		$mongo = MongoFactory::getInstance();
+		$db    = $this->_db;
+		$table = $this->_table;
+		$class = $this->_class;
 
-		$value = $this->_variables;
-		foreach($ex as $idx)
-			$value = $value[$idx];
-
-		return $value;
+		//Find entry
+		$mongo->$db->$table->remove($condition);
 	}
 
-	private function _setDotNotationValue($property, $value)
+	//Function only updates the entry
+	public function update()
 	{
-		$ex  = explode('.', $property);
-		$idx = '';
+		$mongo = MongoFactory::getInstance();
+		$db    = $this->_db;
+		$table = $this->_table;
+		$class = $this->_class;	
 
-		foreach($ex as $ref)
-			$idx .= "['$ref']";
+		//Get variables
+		$vars = $this->_toDotNotation($this->_variables);
+		$cond = array('_id' => $vars['_id']);
+		unset($vars['_id']);
 
-		eval('$this->_variables' . $idx . '=$value;');
-
-		$value = $this->_variables;
-		foreach($ex as $idx)
-			$value = $value[$idx];
-
-		return $value;
+		$set = array('$set' => $vars);
+		$mongo->$db->$table->update($cond, $set);
 	}
 
+	public function save()
+	{
+		$mongo = MongoFactory::getInstance();
+		$save = array();
+
+		foreach($this->_originals as $key => $value)
+		{
+			//Use _variables array since it updates the _originals array
+			if(isset($this->_variables[$key]))
+				$save[$key] = $this->_variables[$key];
+			else
+				$save[$key] = $value;
+		}
+
+		//save the entity
+		$db    = $this->_db;
+		$table = $this->_table;
+
+		//Save to db
+		$mongo->$db->$table->save($save);
+	}
+
+	protected function _getDotNotationValues($notation, &$subject, &$values)
+	{
+		if(!is_array($notation))
+			$notation = explode(".", $notation);
+
+		$key = $notation[0];
+		if($key == "$") //Pivot array inidcator
+		{
+			$val   = &$subject;
+			$count = count($val);
+			array_shift($notation);
+
+			for($i=0; $i<$count; $i++)
+				$this->_getDotNotationValues($notation, $val[$i], $values);
+		}
+		elseif(count($notation) > 1)   //Keep going
+		{
+			//Current key
+			$val = &$subject[$key];
+			array_shift($notation);
+
+			//recursive
+			$this->_getDotNotationValues($notation, $val, $values);
+		}
+		else
+			$values[] = &$subject[$key];
+	}
 
 	private function _typeCastValues()
 	{
 		$casting = $this->_typeCasting();
 		foreach($casting as $cast=>$properties)
 		{
-			if($cast == "int")
+			foreach($properties as $property)
 			{
-				foreach($properties as $property)
-				{
-					$value = $this->_getDotNotationValue($property);
-					$this->_setDotNotationValue($property, (int) $value);
-				}
+				$vals = array();
+				$this->_getDotNotationValues($property, $this->_originals, $vals);
+
+				foreach($vals as &$val)
+					$val = $this->_typeCastValue($cast, $val);
 			}
 		}
+	}
+
+	protected function _typeCastValue($cast, $value)
+	{
+		$cast = strtolower($cast);
+		switch($cast)
+		{
+			case "int":
+				return ((int) $value);
+			
+			case "float":
+				return ((float) $value);
+
+			case "string":
+				return ((string) $value);
+
+			case "boolean":
+			case "bool":
+
+				if(isset($value))
+				{
+					if(is_string($value) && ((strtolower($value) === "true" || strtolower($value) === "on")))
+						return true;
+					elseif(is_numeric($value) && $value != 0)
+						return true;
+					elseif($value === true)
+						return true;
+				}
+
+				return false;
+
+			case "mongodate":
+				if(!is_object($value) && is_numeric($value))
+					return new MongoDate($value);
+				elseif($value === "")
+					return new MongoDate(0);
+
+				return $value;
+
+			case "mongoid":
+				if(!is_object($value))
+					return new MongoId($value);
+
+				return $value;
+		}
+		
+		return $value;
 	}
 
 	protected function _typeCasting()
@@ -68,29 +185,7 @@ class MongoModel extends Model
 		return array();
 	}
 
-	/* Sets up a mongo $set query and returns
-	 * the _id (if one exists) and query array. */
-	public function toQuery($filter=array())
-	{
-		$vars = $this->_variables;
-		unset($vars['_id']);  //take out id
-
-		//Take out anything in the filter
-		if(count($filter))
-		{
-			$keys = array_keys($vars);
-			foreach($filter as $f)
-			{
-				if(in_array($f, $keys))
-					unset($vars[$f]);
-			}
-		}
-
-		$query = $this->_toQuery($vars);
-		return $query;
-	}
-
-	public function _toQuery($vars, $pre="")
+	protected function _toDotNotation($vars, $pre="")
 	{
 		$query = array();
 		foreach($vars as $key=>$value)
@@ -99,7 +194,7 @@ class MongoModel extends Model
 				$key = "$pre.$key";
 
 			if(is_array($value) && count($value))
-				$query = array_merge($query, $this->_toQuery($value, $key));
+				$query = array_merge($query, $this->_toDotNotation($value, $key));
 			elseif(is_array($value))  //empty array
 				$query = array_merge($query, array($key => array()));
 			else
