6c761532dc24545bd6bbabc23878d77c83996dc8
Eric Janik <eric@t3mediainc.com>
2011-04-07 19:15:08 -0500

    added error handling support

---
+++ include/ErrorHandler.php
@@ -0,0 +1,75 @@
+<?
+
+function cocaine_error_handler($errno, $errstr, $errfile, $errline)
+{
+	$summary = <<<HTML
+	<span style="font-size: 20px; color: red;">AN ERROR OCCURRED</span>
+	<br />
+
+	<span style="font-size: 12px; color: red;">You have to fix this error before proceeding.</span>
+	<table id="errorhandler" name="errorhandler" class='summary'>
+		<thead>
+			<th colspan="2">Summary</th>
+		</thead>
+		<tr>
+			<td>File:</td>
+			<td>$errfile</td>
+		</tr>
+		<tr>
+			<td>Line:</td>
+			<td>$errline</td>
+		</tr>
+		<tr>
+			<td>Summary:</td>
+			<td>($errno): $errstr</td>
+		</tr>
+	</table>
+HTML;
+
+	$out = array();
+	$bt  = debug_backtrace();
+	foreach($bt as $b)
+	{
+		if(!isset($b['class']))
+			$b['class'] = "(none)";
+		
+		$out[] = <<<HTML
+		<div>
+			<table id="errorhandler" name="errorhandler" class="detail">
+				<tr>
+					<td>File</td>
+					<td>{$b['file']}</td>
+				</tr>
+				<tr>
+					<td>Class</td>
+					<td>{$b['class']}</td>
+				</tr>
+				<tr>
+					<td>Function</td>
+					<td>{$b['function']}</td>
+				</tr>
+				<tr>
+					<td>Line</td>
+					<td>{$b['line']}</td>
+				</tr>
+			</table>
+		</div>
+HTML;
+	}
+
+	$bt  = '<table id="errorhandler" name="errorhandler" class="stacktrace">';
+	$bt .= '<thead><tr><th colspan=\"2\">Stack Trace</th></tr></thead>';
+	foreach($out as $o)
+	{
+		$bt .= "<tr>";
+		$bt .= "<td>$o</td>";
+		$bt .= "</tr>";
+	}
+	$bt .= "</table>";
+
+	Application::setBackTrace($summary . $bt);
+}
+
+
+set_error_handler('cocaine_error_handler');
+?>
