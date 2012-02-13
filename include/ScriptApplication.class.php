a464b9a97da3ee377406856e3a4d27355089bf8c
Eric Janik <eric@dualclutchmedia.com>
2012-02-13 13:44:06 -0600

    added getApplicationObject static method which chooses the correct language to use for script execution

--- include/ScriptApplication.class.php
+++ include/ScriptApplication.class.php
@@ -19,7 +19,7 @@ abstract class ScriptApplication extends Application
 		$class  = str_replace('_', ' ', $script);
 		$class  = ucwords($class);
 		$class  = str_replace(' ', '', $class);
-		$class  = preg_replace('/\.php$/', '', $class);
+		$class  = preg_replace('/\.(php|js)$/', '', $class);
 		$class .= "Script";
 
 		return $class;
@@ -39,6 +39,46 @@ abstract class ScriptApplication extends Application
 	{
 		echo $message . self::$_LOADING[self::$_LIDX] . "\r";
 		self::$_LIDX = (++self::$_LIDX % count(self::$_LOADING));
+	}
+
+	static public function getApplicationObject($app, $argv)
+	{
+		//Choose proper application wrapper
+		$path = Application::getConfig()->approotpath . "app/scripts/$app/$app";
+		Application::getConfig()->setVariable('scriptrootpath', dirname($path) . "/");
+
+		if(file_exists("$path.php"))
+		{
+			//Require application
+			require_once("$path.php");
+
+			//Create an instance and run the script
+			$class  = ScriptApplication::getClassName($app);
+			$script = new $class($argv);
+		}
+		elseif(file_exists("$path.js"))
+		{
+			//Require Node JS Wrapper
+			require_once(Application::getConfig()->fwrootpath . "include/ScriptNodeJS.class.php");
+
+			//Make sure required nodeJS is available
+			if(!file_exists("/usr/bin/node"))
+			{
+				echo "Cannot run Javascript app because nodejs does not exist in /usr/bin/node!\n";
+				die();
+			}
+
+			//Setup class
+			$class  = ScriptApplication::getClassName($app);
+			$script = new ScriptNodeJS($path . ".js", $class, $argv);
+		}
+		else
+		{
+			echo "Unable to find application script $app.\n";
+			die();
+		}
+
+		return $script;
 	}
 
 	abstract public function run();
