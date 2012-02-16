f713eb55224d21c9742a3c4858edb2e73fb4aa58
Eric Janik <eric@dualclutchmedia.com>
2012-02-16 16:21:41 -0600

    code complete exception handler. code complete view for exception

--- include/GlobalExceptionController.class.php
+++ include/GlobalExceptionController.class.php
@@ -11,7 +11,41 @@ class GlobalExceptionController extends Controller
 
 		//Set view path
 		$this->setViewPath(Application::getConfig()->fwrootpath . 'template/views/');
-		return array('render' => '_global/error', 'layout' => 'error', 'variables' => array('ex' => $this->_ex));
+
+		//Setup 
+		$trace = array();
+		$ex    = $this->_ex;
+		foreach($ex->getTrace() as $t)
+		{
+			$entry = array();
+			$entry['file']    = $t['file'];
+			$entry['line']    = $t['line'];
+			$entry['summary'] = $t['function'];
+
+			if(isset($t['class']))
+				$entry['summary'] = $t['class'] . $t['type'] . $entry['summary'];
+
+			//Get code 
+			$code = explode(PHP_EOL, file_get_contents($t['file']));
+
+			//Grab only 10 lines, with error in center and bolded
+			if(count($code) > 0)
+			{
+				$start = (($t['line'] <= 6)? 0 : $t['line']-5);
+				$nline = $t['line'] - $start;
+				$code  = array_slice($code, $start, 10); //10 lines of code
+
+				$ret = array();
+				foreach($code as $key => $c)
+					$ret[] = array('line' => ($t['line'] + $key) - 6, 'code' => $c, 'selected' => ($key === $nline-1));
+				
+				$entry['code'] = $ret;
+			}
+
+			$trace[] = $entry;
+		}
+
+		return array('render' => '_global/error', 'layout' => 'error', 'variables' => array('scope' => $ex->getScope(), 'message' => $ex->getMessage(), 'file' => $ex->getFile(), 'line' => $ex->getLine(), 'trace' => $trace));
 	}
 
 	public function setException($ex)
--- template/views/_global/error.php
+++ template/views/_global/error.php
@@ -1,5 +1,78 @@
-Arbitrage GlobalErrorController
-<br />
-<pre>
-	<?var_dump($ex);?>
-</pre>
+<script language="JavaScript" type="text/javascript">
+	function toggleCode(idx)
+	{
+		var html = this.innerHTML;
+		console.debug(this.innerHTML);
+		if(html === "[ - ]")
+		{
+			this.innerHTML = "[ + ]";
+			document.getElementById('code_' + idx).style.display = "block";
+		}
+		else
+		{
+			this.innerHTML = "[ - ]";
+			document.getElementById('code_' + idx).style.display = "none";
+		}
+	}
+
+</script>
+
+
+<fieldset>
+	<legend>Arbitrage Exception</legend>
+
+	<h3>Summary:</h3>
+	<div class="summary">
+
+		<div class="entry">
+			<div class="label">Scope</div>
+			<div class="value"><?=$scope;?></div>
+			<div class="clear"></div>
+		</div>
+
+		<div class="entry">
+			<div class="label">Message</div>
+			<div class="value"><?=$message;?></div>
+			<div class="clear"></div>
+		</div>
+
+		<div class="entry">
+			<div class="label">File</div>
+			<div class="value"><?=$file;?></div>
+			<div class="clear"></div>
+		</div>
+
+		<div class="entry">
+			<div class="label">Line</div>
+			<div class="value"><?=$line;?></div>
+			<div class="clear"></div>
+		</div>
+
+	</div>
+
+	<div style="height: 50px"></div>
+
+	<h3>Backtrace:</h3>
+	<div class="backtrace">
+		<?
+		$idx = 0;
+		foreach($trace as $entry)
+		{
+			?>
+			<div class="entry">
+				<div class="expand" onclick="return toggleCode.call(this, <?=$idx?>);">[ - ]</div>
+				<div class="summary"><span style="color: red; font-weight: bold"><?=$entry['summary']?></span> <span style="color: #999999; font-weight: bold">(<?=$entry['file']?>:<?=$entry['line']?>)</span></div>
+				<div class="clear"></div>
+				<div class="code_wrapper" id="code_<?=$idx?>">
+					<?
+					foreach($entry['code'] as $c)
+						echo '<div class="line ' . (($c['selected'])? 'selected' : '') . '">' . $c['line'] . '</div><div class="code ' . (($c['selected'])? 'selected' : '') . '">' . $c['code'] . '</div><div class="clear"></div>';
+					?>
+				</div>
+			</div>
+		<?
+			$idx++;
+		}
+		?>
+	</div>
+</fieldset>
