83384956d000e9c4523da93114ca9bbf925ee115
Eric Janik <eric@dualclutchmedia.com>
2012-02-15 21:02:49 -0600

    updated ArbitrageException class

--- include/ArbitrageException.class.php
+++ include/ArbitrageException.class.php
@@ -1,5 +1,24 @@
 <?
 class ArbitrageException extends Exception
 {
+	static public $UNKNOWN = -1;
+
+	public function getScope()
+	{
+		return "Arbitrage Framework";
+	}
+
+	protected function _mapMessage($err, $vars=NULL)
+	{
+		return "Unknown error occurred.";
+	}
+
+	protected function _replaceFormattedString($str, $vars)
+	{
+		foreach($vars as $key=>$val)
+			$str = preg_replace('/{{' . $key . '}}/', $val, $str);
+
+		return $str;
+	}
 }
 ?>
