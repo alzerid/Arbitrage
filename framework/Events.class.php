455f551529ef83d0d6b4b7a0677772b37ea40de9
Eric Janik <eric@dualclutchmedia.com>
2012-02-22 08:49:55 -0600

    added events classes

---
+++ framework/Events.class.php
@@ -0,0 +1,87 @@
+<?
+
+class CErrorEvent /*extends CEvent*/
+{
+	public $errno;
+	public $errstr;
+	public $message;
+	public $file;
+	public $line;
+	public $trace;
+	public $code;
+
+	public function __construct($errno, $errstr, $message, $file, $line, $trace=NULL)
+	{
+		$this->errno   = $errno;
+		$this->errstr  = $errstr;
+		$this->message = $message;
+		$this->file    = $file;
+		$this->line    = $line;
+
+		//TODO: Possibly remove first trace entry
+		if($trace === NULL)
+		{
+			$trace = debug_backtrace();
+			$trace = array_slice($trace, 2);
+		}
+
+		//Set trace
+		$this->trace = $trace;
+		$this->_getCode();
+	}
+
+	protected function _getCode()
+	{
+		foreach($this->trace as &$trace)
+		{
+			$line    = $trace['line'];
+			$content = explode(PHP_EOL, file_get_contents($trace['file']));
+			$cnt     = count($content);
+
+			//Get range of code
+			if($cnt > 10)
+			{
+				$start = $line - 5;
+				$end   = $line + 5;
+
+				//Normalize start and end
+				if($start < 0)
+					$start = 0;
+				elseif($end >= $cnt)
+					$end = $cnt-1;
+			}
+
+			//Get slice
+			$content = array_slice($content, $start, $end-$start);
+
+			$idx  = $start+1;
+			$code = "";
+			foreach($content as $c)
+			{
+				$class = (($idx == $line)? "selected" : "");
+				$code .= '<div class="line ' . $class . '">' . ($idx) . '</div><div class="code ' . $class . '">' . $c . '</div><div class="clear"></div>';
+				$idx++;
+			}
+
+			$trace['code'] = $code;
+		}
+	}
+}
+
+class CExceptionEvent extends CErrorEvent
+{
+	public $exception;
+
+	public function __construct(Exception $ex)
+	{
+		$this->exception = $ex;
+		$this->errno     = $ex->getCode();
+		$this->errstr    = "";
+		$this->message   = $ex->getMessage();
+		$this->file      = $ex->getFile();
+		$this->line      = $ex->getLine();
+		$this->trace     = $ex->getTrace();
+		$this->_cetCode();
+	}
+}
+?>
