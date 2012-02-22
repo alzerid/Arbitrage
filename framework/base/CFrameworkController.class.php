4816044f9387defbc560d8904cf3cd16b62e4b91
Eric Janik <eric@dualclutchmedia.com>
2012-02-22 08:51:29 -0600

    added the CFrameworkController class which is specific to framework rendering functionality

---
+++ framework/base/CFrameworkController.class.php
@@ -0,0 +1,26 @@
+<?
+class CFrameworkController extends CBaseController
+{
+	public function render($file, $_vars=NULL)
+	{
+		if($_vars === NULL)
+			$_vars = array();
+
+		//Extract variables
+		extract($_vars);
+
+		//Generate file path
+		$path = CApplication::getConfig()->_internals->fwrootpath . "framework/views/exception.php";
+
+		ob_start();
+		require_once($path);
+		$content = ob_get_clean();
+
+		return $content;
+	}
+
+	public function renderInternal(IRenderer $renderer)
+	{
+	}
+}
+?>
