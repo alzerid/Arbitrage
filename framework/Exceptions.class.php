f5e8b47bffa697a8aecc9757648365bbb64bf01a
Eric Janik <eric@dualclutchmedia.com>
2012-02-22 14:41:36 -0600

    slimmed down some Exception classess

--- framework/Exceptions.class.php
+++ framework/Exceptions.class.php
@@ -1,17 +1,9 @@
 <?
 class EArbitrageException extends Exception
 {
-	protected $_scope;
-
-	public function __construct($scope = "Arbitrage Framework", $message="", $code=0, $previous=NULL)
+	public function __construct($message="", $code=0, $previous=NULL)
 	{
 		parent::__construct($message, $code, $previous);
-		$this->_scope = $scope;
-	}
-
-	public function getScope()
-	{
-		return $this->_scope;
 	}
 }
 
@@ -19,7 +11,7 @@ final class PHPException extends EArbitrageException
 {
 	public function __construct($message, $code, $file, $line, $previous=NULL)
 	{
-		parent::__construct("PHP Error", $message, $code, $previous);
+		parent::__construct($message, $code, $previous);
 		$this->file   = $file;
 		$this->line   = $line;
 		$this->_scope = "PHP Error";
@@ -28,17 +20,9 @@ final class PHPException extends EArbitrageException
 
 final class EArbitrageConfigException extends EArbitrageException
 {
-	public function __construct($message="", $code=0, $previous=NULL)
-	{
-		parent::__construct("Arbitrage Configuration Exception", $message, $code, $previous);
-	}
 }
 
 final class EArbitrageRemoteCacheException extends EArbitrageException
 {
-	public function __construct($message="", $code=0, $previous=NULL)
-	{
-		parent::__construct("Arbitrage Remote Cache Exception", $message, $code, $previous);
-	}
 }
 ?>
