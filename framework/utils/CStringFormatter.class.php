1c992fb83d856a2a54eb7651c8c6611774ad5c34
Eric Janik <eric@dualclutchmedia.com>
2012-04-17 09:03:37 -0500

    added number precision functionality to CStringFormatter

--- framework/utils/CStringFormatter.class.php
+++ framework/utils/CStringFormatter.class.php
@@ -13,12 +13,18 @@ class CStringFormatter
 		//Find all %X
 		$matches = array();
 		$format  = $this->_format;
-		preg_match_all('/%[a-zA-Z]/s', $this->_format, $matches);
+		preg_match_all('/%(\.[0-9]*)?[a-zA-Z]/s', $this->_format, $matches);
+		$numbers = ((isset($matches[1]))? $matches[1] : array());
 
-		foreach($matches[0] as $m)
+		foreach($matches[0] as $idx=>$m)
 		{
-			$key = substr($m, 1);
-			if(isset($vals[$key]))
+			$key = preg_replace('/^%.*([A-Za-z])$/', '$1', $m); 
+			if(isset($vals[$key]) && !empty($numbers[$idx]))
+			{
+				$val    = $this->_formatNumber($vals[$key], $numbers[$idx]);
+				$format = preg_replace('/' . $m . '/', $val, $format);
+			}
+			elseif(isset($vals[$key]))
 			{
 				$val    = $vals[$key];
 				$format = preg_replace('/' . $m . '/', $vals[$key], $format);
@@ -26,6 +32,13 @@ class CStringFormatter
 		}
 
 		return $format;
+	}
+
+	private function _formatNumber($value, $format)
+	{
+		$format = "%" . $format . 'f';
+		$value  = floatval($value);
+		return sprintf($format, $value);
 	}
 }
 ?>
