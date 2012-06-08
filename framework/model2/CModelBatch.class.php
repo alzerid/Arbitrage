f5954d2921bb0160a32328337a1c1168581ee2b6
Eric Janik <eric@dualclutchmedia.com>
2012-06-08 08:35:04 -0500

    modified execute method to throw EModelException since it should be overloaded

--- framework/model2/CModelBatch.class.php
+++ framework/model2/CModelBatch.class.php
@@ -5,12 +5,7 @@ abstract class CModelBatch extends CModelQuery
 {
 	public function execute()
 	{
-		if($this->_cmd == "insert")
-		{
-			die("INSERT");
-		}
-		else
-			throw new EModelExceptoin("Cannot do batch operation on '{$this->_cmd}'.");
+		throw new EModelException("Cannot do batch operation on '{$this->_cmd}'.");
 	}
 }
 ?>
