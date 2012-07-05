26f4c5beac38b62d91081f72bbea408af6d1fb59
Eric Janik <eric@dualclutchmedia.com>
2012-07-05 09:56:34 -0500

    added code to properly render CHTMLDIvDataTable

--- framework/html/CHTMLDivDataTable.class.php
+++ framework/html/CHTMLDivDataTable.class.php
@@ -1,9 +1,59 @@
 <?
 class CHTMLDivDataTable extends CHTMLDataTable
 {
+	public function __construct($id, $headers, $data, $attrs=array())
+	{
+		parent::__construct($id, $headers, $data, $attrs);
+	}
+	
 	public function render()
 	{
+		$attrs = CHTMLComponent::generateAttribs($this->_attrs);
+		$html  = "<div id=\"{$this->_id}\" $attrs>";
+			$html .= $this->_renderHeader();
+			$html .= $this->_renderData();
+		$html .= "</div>";
+
 		die("IMPLEMENT ME");
+	}
+
+	protected function _renderHeader()
+	{
+		$html = '<div class="header">';
+		foreach($this->_headers as $title => $val)
+			$html = '<div class="entry">' . $val . '</div>';
+
+		$html .= '</div>';
+
+		return $html;
+	}
+
+	protected function _renderData()
+	{
+		$html = '<div class="dataset">';
+			
+		//Go thorugh each entry
+		foreach($this->_data as $entry)
+		{
+			//Add cell
+			foreach($this->_headers as $key => $val)
+			{
+				$html .= "<div class=\"cell\">";
+				if(gettype($val) === "string")
+				{
+					$arr = new CArrayObject($entry);
+					$val = $this->_normalizeValue($arr->xpath($val));
+				}
+				else($val instanceof IHTMLDataTableEntry)
+					$val = $val->render($this, $entry);
+
+				$html .= "<div class=\"cell\">$val</div>";
+			}
+
+			$html .= "</div>";
+		}
+		
+		$html .= "</div>";
 	}
 }
 ?>
