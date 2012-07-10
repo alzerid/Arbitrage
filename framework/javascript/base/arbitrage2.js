b8540085887ba2783c7c9a3f256f436e17ad1a16
Eric Janik <eric@dualclutchmedia.com>
2012-07-10 11:06:26 -0500

    refactoring code

--- framework/javascript/base/arbitrage2.js
+++ framework/javascript/base/arbitrage2.js
@@ -8,7 +8,7 @@
 if(!window['$l'])
 {
 	var $l = function() {
-		if($.cookie('debug') == 1 && console && console.log)
+		if($.cookie('debug') == 1 && arbitrage2.config.log && console && console.log)
 		{
 			if(console.log.apply)
 			{
@@ -49,20 +49,34 @@ arbitrage2.inherit = function(child, parent) {
 };
 
 /**
-	@description Private variable counting how many modules are still being loaded.
+	@description Private variable counting how many javascript files are still being loaded.
 */
-arbitrage2._loading = 0;
+arbitrage2._loading_javascripts = 0;
 
 /**
-	@description List of required files being loaded or already loaded.
+	@description List of javascript required files and their associative states.
 	@static
 */
-arbitrage2._requiring = [ ];
+arbitrage2._required_javascripts = [ ];
+
+/**
+	@description List of stylesheet required files and their associative states.
+*/
+arbitrage2._required_stylesheets = [ ];
 
 /**
 	@description Called when the arbitrage2 application is ready.
 */
 arbitrage2.ready = false;
+
+/**
+	@description Arbitrage2 config.
+*/
+arbitrage2.config = arbitrage2.config || {
+	javascriptPath: "/javascript",           //Javascript path to use when including files
+	stylesheetPath: "/stylesheets",          //Stylesheet path to use when including css
+	mvc: { }                                 //Empty config only used when MVC pattern is used
+};
 
 /**
 	@description Tells Arbitrage2 that the file being implemented is providng a certain namespace. Implments object into the global namespace.
@@ -83,17 +97,157 @@ arbitrage2.provide = function(namespace) {
 };
 
 /**
+	@description Require file class that holds relavent information and methods.
+	@params opt_cb_load Optional onload callback.
+	@params opt_cb_error Optional onload error callback.
+*/
+arbitrage2.RequiredFile = function(opt_cb_load, opt_cb_error) {
+	var self       = this;
+	self.state     = "requiring";   //The state the file is in
+	self.element   = undefined;     //The DOM element associated with the RequiredFile  
+	self.cb_load   = [ ];           //The array of callback functions to call when loaded
+	self.cb_error  = [ ];           //The array of callback functions to call when an error occurs
+	self.namespace = undefined;     //Associated namespace if relavent
+
+	//Loading variables
+	self.js_loading  = 0;
+	self.css_loading = 0;
+
+	//Events setup
+	if(opt_cb_load)
+		self.cb_load.push(opt_cb_load);
+	
+	if(opt_cb_error)
+		self.cb_error.push(opt_cb_error);
+};
+
+/**
+	@description Static variable keeping track of all required javascript files.
+	@static
+*/
+arbitrage2.RequiredFile.javascripts = [ ];
+
+/**
+	@description Static variable keeping track of all required stylesheet files.
+	@static
+*/
+arbitrage2.RequiredFile.stylesheets = [ ];
+
+
+/**
+	@description Adds an SCRIPT element to the DOM.
+*/
+arbitrage2.RequiredFile.prototype.loadJavascript = function() {
+	var self = this;
+
+	//Add to javascripts
+	arbitrage2.RequiredFile.javascripts.push(self);
+	arbitrage2.RequiredFile.
+	
+	/*arbitrage2._required_javascripts.push(script);
+
+	//Create javascript tag
+	script.element = document.createElement('script');
+
+	//setup script attributes
+	script.element.type     = 'text/javascript';
+	script.element.language = "JavaScript";
+	script.element.src      = path;
+	script.element.async    = false;
+
+	//Setup state change events
+	if('onreadystatechange' in script.element) //IE browsers
+	{
+		alert('state change browser');
+		/*requiring.element.onerror = function(ev) {
+			if(opt_cb_error)
+				opt_cb_error();
+		};
+
+		requiring.element.onreadystatechange = function(ev) {
+			if(this.readyState == "complete" || this.readyState == 'loaded')
+				_checkReady();
+		};*/
+	/*}
+	else
+	{
+		script.element.onload  = _cbLoaded();
+		script.element.onerror = _cbError();
+	}
+
+	//Add to loading
+	arbitrage2._loading_javascripts++;
+	$l('requiring "' + path + '"', arbitrage2._loading_javascripts, arbitrage2.ready);
+
+	//Set state
+	script.state = 'loading';
+	
+	//Add to HEAD
+	document.getElementByTagName('head').item(0).appendChild(script.element);*/
+};
+
+
+/**
+	@description Method appends callbacks to the callback list. If the script is already loaded, we immediately call the callback and return.
+	@params opt_cb_load Optional onload callback.
+	@params opt_cb_error Optional onload error callback.
+	@return Returns true if a callback was immediately called, else false.
+*/
+arbitrage2.RequiredFile.prototype.appendCallbacks = function(opt_cb_load, opt_cb_error) {
+	var self = this;
+
+	if(script.state == "loaded" || script.state == "error")
+	{
+		if(opt_cb_load)
+			opt_cb_load.call(self);
+
+		return true;
+	}
+	else if(script.state == "error")
+	{
+		if(opt_cb_error)
+			opt_cb_error.call(self);
+
+		return true;
+	}
+	else
+	{
+		if(opt_cb_load)
+			self.cb_load.push(opt_cb_load);
+
+		if(opt_cb_error)
+			self.cb_error.push(opt_cb_error);
+	}
+
+	return false;
+};
+
+
+/**
+	@description Requires a javascript dependency file into the DOM.
+	@param file The file to include.
+	@params opt_cb_load Optional onload callback.
+	@params opt_cb_error Optional onload error callback.
+*/
+arbitrage2.requireJavascript = function(file, opt_cb_load, opt_cb_error) {
+
+	//Include script
+	var script = new arbitrage2.RequiredFile(opt_cb_load, opt_cb_error);
+	script.loadJavascript();
+};
+
+/**
 	@description Requires a dependency file.
 	@params namespace The namespace to requires.
 	@params opt_cb_load Optional onload callback.
 	@params opt_cb_error Optional onload error callback.
 */
 arbitrage2.require = function(namespace, opt_cb_load, opt_cb_error) {
-	var self = this;
 
-	function _checkReady() {
+	/*function _checkReady() {
 
 		arbitrage2._loading--;
+		this.state = 'loaded';
 
 		if(arbitrage2._loading == 0)
 			arbitrage2.ready = true;
@@ -102,11 +256,12 @@ arbitrage2.require = function(namespace, opt_cb_load, opt_cb_error) {
 
 		//User callback
 		if(opt_cb_load)
-			opt_cb_load.call(this);
+			opt_cb_load.call(this.element);
 	};
 
 	function _markError() {
 		arbitrage2._loading--;
+		this.state = 'error';
 
 		if(arbitrage2._loading == 0)
 			arbitrage2.ready = true;
@@ -115,68 +270,52 @@ arbitrage2.require = function(namespace, opt_cb_load, opt_cb_error) {
 
 		//User callback
 		if(opt_cb_error)
-			opt_cb_error.call(this);
-	};
+			opt_cb_error.call(this.element);
+	};*/
 
-	var file      = namespace.split('.');
-	var namespace = file.splice(0, file.length-1);
-	var split     = file.join('').match(/[A-Z][a-z]+/g);
-	if(split)
-		file = split.join('_').toLowerCase();
 
-	var path = "/javascript/" + namespace.join('/') + "/" + file + ".js";
-
-	//Check if element already exists, if so we pretend we required it again
-	for(var i=0, script; i<self._requiring.length, script=self._requiring[i]; i++)
+	//Ensure symbol does not exist
+	var symbol = arbitrage2.getSymbol(namespace);
+	if(symbol)
 	{
-		if(script == path)
+		for(var i=0, script; i<arbitrage2._required_javascripts.length, script=arbitrage2._required_javascripts[i]; i++)
 		{
-			$l('script already requiring or required ' + script);
-			if(opt_cb_load)
-				opt_cb_load.call();
-
-			return;
+			if(script.namespace == namespace)
+			{
+				if(script.appendCallbacks(opt_cb_load, opt_cb_error))
+					return
+			}
 		}
 	}
 
-	//Add to requiring
-	self._requiring.push(path);
+	//Require the javascript file and add to _required_javascipts
+	var onamespace = namespace;
+	var file       = namespace.split('.');
+	var namespace  = file.splice(0, file.length-1);
+	var split      = file.join('').match(/[A-Z][a-z]+/g);
+	if(split)
+		file = split.join('_').toLowerCase();
+	
+	var path = arbitrage2.config.javascriptPath + "/" + namespace.join('/') + "/" + file + ".js";
 
-	//Create javascript tag
-	var head   = document.getElementsByTagName('head').item(0);
-	var script = document.createElement('script');
+	//Require the JS File in a closure to retain namespacing
+	(function(namespace, path, opt_cb_load, opt_cb_error) {
 
-	//setup script attributes
-	script.type     = 'text/javascript';
-	script.language = "JavaScript";
-	script.src      = path;
+		function _cbLoad() {
+			this.namespace = namespace;
+			if(opt_cb_load)
+				opt_cb_load.call(this);
+		};
 
-	if('onreadystatechange' in script)
-	{
-		script.onerror = function(ev) {
+		function _cbError() {
+			this.namespace = namespace;
 			if(opt_cb_error)
-				opt_cb_error();
+				opt_cb_error.call(this);
 		};
 
-		script.onreadystatechange = function(ev) {
-			if(this.readyState == "complete" || this.readyState == 'loaded')
-				_checkReady();
-		};
-	}
-	else
-	{
-		script.onload  = _checkReady;
-		script.onerror = _markError;
-	}
+		arbitrage2.requireJavascript(path, _cbLoad, _cbError);
 
-	script.async = false;
-	
-	//Add to loading
-	arbitrage2._loading++;
-	$l('requiring "' + path + '"', arbitrage2._loading, arbitrage2.ready);
-	
-	//Add to dom
-	head.appendChild(script);
+	})(onamespace, path, opt_cb_load, opt_cb_error);
 };
 
 /**
@@ -213,7 +352,7 @@ arbitrage2.getSymbol = function(namespace) {
 	namespace = namespace.split('.');
 	
 	var symbol = window;
-	for(var i=0; symbol=symbol[namespace[i]], i<namespace.length-1; i++);
+	for(var i=0; symbol=symbol[namespace[i]], i<namespace.length-1, symbol; i++);
 
 	return symbol;
 };
