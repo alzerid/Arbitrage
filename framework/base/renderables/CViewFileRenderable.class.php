c98c2ce1a6cf158001d52a577eb66d42ec8620bf
Eric Janik <eric@dualclutchmedia.com>
2012-07-17 18:17:55 -0500

    moved all rendering code into CViewFileRenderable

--- framework/base/renderables/CViewFileRenderable.class.php
+++ framework/base/renderables/CViewFileRenderable.class.php
@@ -2,68 +2,91 @@
 class CViewFileRenderable implements IViewFileRenderable
 {
 	protected $_view_variables;
-	protected $_layout_path;
-	protected $_view_path;
-	protected $_default_layout;
-	protected $_renderable;
+	protected $_layout_paths;
+	protected $_view_paths;
+	protected $_layout;
 
 	public function __construct()
 	{
-		//setup view and layout paths
-		$this->setLayoutPath();
-		$this->setViewPath();
-		$this->setDefaultLayout();
+		$this->_layout_paths = array();
+		$this->_view_paths   = array();
 
-		$this->_renderable    = array();
+		//setup view and layout paths
+		$this->addLayoutPath(CApplication::getConfig()->_internals->approotpath . "app/views/layout/");
+		$this->addViewPath(CApplication::getConfig()->_internals->approotpath . "app/views/");
+		$this->setLayout("default");
+
+		//$this->_renderable    = array();
 		$this->_view_variables = array();
 	}
 
-	public function setDefaultLayout($layout="default")
+	public function setLayout($layout)
 	{
-		$this->_default_layout = $layout;
+		$this->_layout = $layout;
 	}
 
-	public function setLayoutPath($path=NULL)
+	public function getLayout()
 	{
-		$this->_layout_path = (($path === NULL)? CApplication::getConfig()->_internals->approotpath . "app/views/layout/" : $path);
-	}
-
-	public function setViewPath($path=NULL)
-	{
-		$this->_view_path = (($path === NULL)? CApplication::getConfig()->_internals->approotpath . "app/views/" : $path);
-	}
-
-	public function getDefaultLayout()
-	{
-		return $this->_default_layout;
+		return $this->_layout;
 	}
 
 	public function getLayoutPath()
 	{
-		return $this->_layout_path;
+		return $this->_layout_paths;
 	}
 
-	public function getViewPath()
+	public function addLayoutPath($path)
 	{
-		return $this->_view_path;
+		$this->_layout_paths[] = $path;
 	}
 
-	public function render()
+	public function getViewPaths()
 	{
+		return $this->_view_paths;
+	}
+
+	public function addViewPath($path=NULL)
+	{
+		$this->_view_paths[] = $path;
+	}
+
+	public function render($data=NULL)
+	{
+		//Setup data
+		$default = array('render'    => CApplication::getInstance()->getController()->getName() . "/" . CApplication::getInstance()->getController()->getAction()->getName(),
+		                 'layout'    => $this->_layout,
+		                 'variables' => array());
+
+		//Merge defaults with data
+		$default['render']    = ((isset($data['render']))? $data['render'] : $default['render']);
+		$default['layout']    = ((isset($data['layout']))? $data['layout'] : $default['layout']);
+		$default['variables'] = array_merge($default['variables'], (isset($data['variables'])? $data['variables'] : array()));
+
 		//Get content from view
-		$content = $this->renderPartial($this->_renderable['render'], $this->_view_variables);
+		$content = $this->renderPartial($default['render'], $default['variables']);
 
 		//Now render layout
-		$path = $this->_layout_path . $this->_renderable['layout'] . ".php";
-		if(!file_exists($path))
-			throw new EArbitrageException("Layout does not exist '$path'.");
+		$layout = NULL;
+		foreach($this->_layout_paths as $lp)
+		{
+			if(file_exists($lp . "/" . $default['layout'] . ".php"))
+			{
+				$layout = $lp . "/" . $default['layout'] . ".php";
+				break;
+			}
+		}
+
+		if(!file_exists($layout))
+			throw new EArbitrageException("Layout does not exist '{$default['layout']}'.");
 
 		//Extract the variables
 		$_vars = $this->_view_variables;
 		extract($_vars);
 
 		//Require view
-		require_once($path);
+		ob_start();
+		ob_implicit_flush(false);
+		require_once($layout);
 
 		return ob_get_clean();
 	}
@@ -74,11 +97,19 @@ class CViewFileRenderable implements IViewFileRenderable
 		if($_vars !== NULL)
 			extract($_vars);
 
-		//Generate file path
-		$path = $this->_view_path . $file . ".php";
+		//Get view file
+		$path = NULL;
+		foreach($this->_view_paths as $vp)
+		{
+			if(file_exists($vp . "/" . $file . ".php"))
+			{
+				$path = $vp . "/" . $file . ".php";
+				break;
+			}
+		}
 		
-		if(!file_exists($path))
-			throw new EArbitrageException("View file does not exist '$path'.");
+		if($path == NULL)
+			throw new EArbitrageException("View file does not exist '$file'.");
 
 		ob_start();
 		ob_implicit_flush(false);
@@ -86,7 +117,6 @@ class CViewFileRenderable implements IViewFileRenderable
 		$content = ob_get_clean();
 
 		return $content;
-
 	}
 }
 ?>
