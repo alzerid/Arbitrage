fbe2f369f66157d970fd47b4d997d3730043b7bf
Eric Janik <eric@dualclutchmedia.com>
2012-07-19 08:45:53 -0500

    updated CExtension class

--- framework/base/CExtension.class.php
+++ framework/base/CExtension.class.php
@@ -5,12 +5,13 @@ class CExtension
 {
 	protected $_name;
 	protected $_path;
-
+	protected $_config;
 
 	public function __construct($path, $name)
 	{
-		$this->_path = $path;
-		$this->_name = $name;
+		$this->_path   = $path;
+		$this->_name   = $name;
+		$this->_config = NULL;
 	}
 
 	public function initialize()
@@ -21,11 +22,22 @@ class CExtension
 		if(count($file) == 0)
 			throw new EArbitrageException("Unable to load config file for extension '{$this->_name}'.");
 
+		//Load config for extension
 		$config->initialize(dirname($file[0]));
 		$config->load(basename($file[0]));
+		$config = $config->toArray();
+
+		//Get ext config
+		$this->_config = new \CArbitrageConfigProperty($config['extension']);
+		unset($config['extension']);
 
 		//Merge this new config with the application config
-		\CApplication::getConfig()->merge($config->toArray());
+		\CApplication::getConfig()->merge($config);
+	}
+
+	public function getConfig()
+	{
+		return $this->_config;
 	}
 }
 ?>
