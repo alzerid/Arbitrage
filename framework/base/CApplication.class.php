1b2131f29e9de91cc0da45166269132400ff0e73
Eric Janik <eric@dualclutchmedia.com>
2012-07-26 15:34:08 -0500

    added initializeServices and intializePackages capbilities. add the getPackage method

--- framework/base/CApplication.class.php
+++ framework/base/CApplication.class.php
@@ -3,18 +3,32 @@ namespace Arbitrage2\Base;
 
 abstract class CApplication extends CPackage implements \Arbitrage2\Interfaces\IErrorHandlerListener
 {
+	private $_packages;            //A list of packages registered to this application
+
 	/**
 	 * Initializes the arbitrage application, loads the application config.
-	 * @param string $path The path where the application resides in.
-	 * @param string $namespace The namespace associated with the object.
 	 */
-	public function initialize($path, $namespace)
+	public function initialize()
 	{
+		//Set arrays
+		$this->_packages = array();
+
 		//Setup error handler
 		\Arbitrage2\ErrorHandler\CErrorHandlerObserver::getInstance()->addListener($this);
 
 		//Call CPackage::initialize
-		parent::initialize($path, $namespace);
+		parent::initialize();
+	}
+
+	/**
+	 * Method returns a package based on namespace.
+	 * @param string $namespace The namespace to retrieve.
+	 * @return \Arbitrage\Base\CPackage The returned package.
+	 */
+	public function getPackage($namespace)
+	{
+		$key = strtolower($namespace);
+		return ((isset($this->_packages[$key]))? $this->_packages[$key] : NULL);
 	}
 
 	/**
@@ -22,6 +36,36 @@ abstract class CApplication extends CPackage implements \Arbitrage2\Interfaces\I
 	 * upon application execution.
 	 */
 	abstract public function run();
+
+	/**
+	 * Method that initializes services.
+	 */
+	protected function _initializeServices()
+	{
+		//Call parent initialize services
+		CKernel::getInstance()->initializeServices($this);
+	}
+
+	/**
+	 * Method initializes packages.
+	 */
+	protected function _initializePackages()
+	{
+		//Get arbitrage2 config
+		$config = $this->getConfig();
+		if($config->arbitrage2 && $config->arbitrage2->packages)
+		{
+			//Grab packages
+			$packages = $config->arbitrage2->packages;
+
+			//Create package
+			foreach($packages as $package => $lconfig)
+			{
+				$key = preg_replace('/\.[^\.]+$/', '', strtolower($package));
+				$this->_packages[$key] = CKernel::getInstance()->createPackage($package, $this, new \Arbitrage2\Config\CArbitrageConfigProperty($lconfig));
+			}
+		}
+	}
 
 	/**
 	 * Method handles errors.
