fdc189ac8070f24dbe24faaafff08ac62e2dff7d
Eric Janik <eric@dualclutchmedia.com>
2012-07-27 09:36:00 -0500

    updated script tag implementation

--- framework/client_mvc/CClientMVCPackage.class.php
+++ framework/client_mvc/CClientMVCPackage.class.php
@@ -10,21 +10,22 @@ class CClientMVCPackage extends \Framework\Base\CPackage
 		//Get config and update routing
 		$routes = $this->getRootParent()->getConfig()->webApplication->routes;
 		$array  = $routes->toArray();
+		$app    = $this->getApplication();
 
 		//Prepend
 		$new_routes = array('/^\/bootstrap\.js(\?.*)?$/i'                    => 'framework/client_mvc/client/bootstrap',
 		                    '/^\/framework\/client_mvc\/javascript\/.*.js$/' => 'framework/client_mvc/client/getJavascript');
 
 		//Update routes
-		$this->getRootParent()->getConfig()->webApplication->routes = array_merge($new_routes, $array);
+		$app->getConfig()->webApplication->routes = array_merge($new_routes, $array);
 
 		//Add bootstrap.js and arbitrage javascript tags
-		\Framework\DOM\CDOMGenerator::addJavascriptTag(array('src' => '/bootstrap.js?action=' . $this->getRootParent()->getURI()));
+		\Framework\DOM\CDOMGenerator::addJavascriptTag(array('src' => '/bootstrap.js?action=' . $app->getVirtualURI()));
 		\Framework\DOM\CDOMGenerator::addJavascriptTag(array('src' => '/framework/client_mvc/javascript/arbitrage2/base/arbitrage2.js'));
 
 		//require javascript file defined by user
-		if(isset($this->getConfig()->applicationScript))
-			\Framework\DOM\CDOMGenerator::addJavascriptTag(array('src' => $this->getConfig()->applicationScript));
+		if(isset($this->getConfig()->applicationPath))
+			\Framework\DOM\CDOMGenerator::addJavascriptTag(array('src' => $this->getConfig()->applicationPath . "/application.js"));
 	}
 }
 ?>
