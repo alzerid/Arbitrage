f545e8fe74656250f2993363378485fdd121da97
Eric Janik <eric@dualclutchmedia.com>
2012-08-02 13:39:52 -0500

    updated CDatabaseService with getDriver and handleAutoLoad methods

--- framework/database/CDatabaseService.class.php
+++ framework/database/CDatabaseService.class.php
@@ -2,10 +2,11 @@
 namespace Framework\Database;
 use \Framework\Base\CService;
 
-class CDatabaseService extends CService
+class CDatabaseService extends CService implements \Framework\Interfaces\IAutoLoadListener
 {
 	static protected $_SERVICE_TYPE = "database";   //Service type
 	protected $_db_config;                          //Database configuration
+	protected $_drivers;                            //Driver list
 
 	/**
 	 * Method initializes the service.
@@ -20,11 +21,12 @@ class CDatabaseService extends CService
 		$this->requireServiceFile("CModelHashData");
 		$this->requireServiceFile("CModelQuery");
 		$this->requireServiceFile("CModelBatch");
-		$this->requireServiceFile("CModel");
+		$this->requireServiceFile("CModel", array('_service' => $this));
 		$this->requireServiceFile("CDatabaseDriver");
 
 		//TODO: Load necessary drivers
 		$this->_db_config = array();
+		$this->_drivers   = array();
 		$config           = $this->getConfig();
 		$loaded           = array();
 		foreach($config as $key => $val)
@@ -39,25 +41,70 @@ class CDatabaseService extends CService
 				$this->requireServiceFile("$namespace.C{$namespace}ModelBatch");
 				$this->requireServiceFile("$namespace.C{$namespace}ModelResults");
 
+				//Add to loaded
 				$loaded[] = $val['driver'];
 			}
 		}
 
-		//Setup model autoload
-		spl_autoload_register(__NAMESPACE__ . '\CDatabaseService::modelAutoLoad', true, true);
+		//TODO: Register this service to the application autoload events
+		$this->getApplication()->registerAutoLoadListener($this);
 	}
 
 	/**
-	 * Method autoloads model files.
+	 * Method returns a driver for DB access functionality.
+	 * @param array $opt_prop The properties to merge with.
+	 * @returns array The resulting driver.
 	 */
-	static public function modelAutoLoad($class)
+	public function getDriver($opt_prop=array())
 	{
-		//Make sure we are model autoloading
-		if(preg_match('/Model$/', '', $class))
+		$key    = ((isset($opt_prop['config']))? $opt_prop['config'] : '_default');
+		$config = $this->getConfig();
+
+		if(!isset($config[$key]))
+			throw new EDatabaseDriverException("Unable to load config for '$key'.");
+
+		//Merge properties
+		$config = array_merge($config[$key]->toArray(), $opt_prop);
+		$driver = $config['driver'];
+		$key    = "$driver.{$config['host']}.{$config['port']}";
+
+		//Get driver
+		if(isset($this->_drivers[$key]))
+			return $this->_drivers[$key];
+
+		//Create driver
+		$class                = '\\Framework\\Database\\C' . $config['driver'] . 'Driver';
+		$this->_drivers[$key] = new $class($config);
+
+		return $this->_drivers[$key];
+	}
+
+	/**
+	 * Method listens for autoload events.
+	 * @param \Framework\Interfaces\IEvent $event The event object containing event information.
+	 */
+	public function handleAutoLoad(\Framework\Interfaces\IEvent $event)
+	{
+		static $loaded = array();
+
+		if(preg_match('/Model$/', $event->class))
 		{
-			$file = preg_replace('/Model$/', '', $class);
-			var_dump($file);
-			die();
+			//Check if we already loaded
+			$class = "\\{$event->class}";
+			if(isset($loaded[$class]))
+				return;
+
+			//Require the file
+			$namespace = explode('.', \Framework\Base\CKernel::getInstance()->convertPHPNamespaceToArbitrage(preg_replace('/Model$/', '', $class)));
+			$namespace = implode('.', array_slice($namespace, 0, -1)) . "." . strtolower($namespace[count($namespace)-1]);
+			$ret       = \Framework\Base\CKernel::getInstance()->requireFile($namespace, false);
+
+			//Throw exception if unable to load model
+			if(!$ret)
+				throw new EDatabaseDriverException("Unable to load model '$namespace'.");
+
+			//Loaded, prevent from propagating
+			$event->stopPropagation();
 		}
 	}
 }
