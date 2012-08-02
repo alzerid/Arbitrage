3894e2c5a9dbdb7e01660f758767d620efe9206a
Eric Janik <eric@dualclutchmedia.com>
2012-08-02 13:40:53 -0500

    updated method to use new parameters

--- framework/database/CModelBatch.class.php
+++ framework/database/CModelBatch.class.php
@@ -3,7 +3,7 @@ namespace Framework\Database;
 
 abstract class CModelBatch extends CModelQuery
 {
-	public function execute()
+	public function execute(\Framework\Database\CModelResults $results)
 	{
 		throw new EModelException("Cannot do batch operation on '{$this->_cmd}'.");
 	}
