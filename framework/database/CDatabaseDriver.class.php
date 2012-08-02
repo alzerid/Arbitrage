ea08675998ba9a2e2b30062d45e4c8e78bbeb3cd
Eric Janik <eric@dualclutchmedia.com>
2012-08-02 13:38:46 -0500

    converted CDatabaseDriver to a instance class instead of a static class.

--- framework/database/CDatabaseDriver.class.php
+++ framework/database/CDatabaseDriver.class.php
@@ -2,9 +2,49 @@
 namespace Framework\Database;
 abstract class CDatabaseDriver
 {
-	static public function getHandle($config)
+	protected $_handle;
+	protected $_config;
+
+	/**
+	 * Class holds the handle and other information for the driver connection.
+	 * @param $config The config to use for connection purposes.
+	 */
+	public function __construct($config)
 	{
-		throw new \Framework\Interfaces\EDatabaseDriverException("Your driver must implement ::getHandle.");
+		$this->_config = $config;
 	}
+
+	/**
+	 * Method returns the raw handle of the driver.
+	 * @return Returns the handle.
+	 */
+	public function getHandle()
+	{
+		return $this->_handle;
+	}
+
+	/**
+	 * Method retuns the configuration of this driver.
+	 * @returns array Returns driver configuration.
+	 */
+	public function getConfig()
+	{
+		return $this->_config;
+	}
+
+	/*
+	 * Abstract method retuns the form object.
+	 */
+	abstract public function getForm(array $form);
+
+	/**
+	 * Abstract method returns the correct Query class.
+	 */
+	abstract public function getQuery($class);
+
+	/**
+	 * Abstract method reutns a batch object.
+	 */
+	abstract public function getBatch();
 }
 ?>
