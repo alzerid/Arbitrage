306be054f802295f9cab6da4b1c2f34e07cbe18b
Eric Janik <eric@dualclutchmedia.com>
2012-08-07 18:04:15 -0500

    added initializeJavascript and addRoute

--- framework/base/CWebPackage.class.php
+++ framework/base/CWebPackage.class.php
@@ -3,32 +3,47 @@ namespace Framework\Base;
 class CWebPackage extends CPackage
 {
 	/**
-	 * Initializes the web package.
+	 * Initialize JS the package.
+	 * @param string The controller to use for Javascript Return.
 	 */
-	public function initialize()
+	public function initializeJavascript($controller)
 	{
-		parent::initialize();
+		//Create URL
+		$base = CKernel::getInstance()->convertArbitrageNamespaceToURL($this->getNamespace());
+		$url  = "/$base/javascript/";
+		$url  = "/^" . preg_replace('/\//', '\/', $url) . ".*.js$/";
 
-		//Add Javascript routing capabilities
-		$namespace = $this->getNamespace();
-
-		//Update routes
-		$routes = $this->getRootParent()->getConfig()->webApplication->routes->toArray();
-		$url    = \Framework\Base\CKernel::getInstance()->convertArbitrageNamespaceToURL($this->getNamespace());
-
-		//Prepend
-		$new_route = array('/^\/' . preg_replace('/\//', '\/', $url) . '\/javascript\/.*.js$/' => $url . '/blog/getJavascript');
-
-		//Update routes
-		$this->getApplication()->getConfig()->webApplication->routes = array_merge($new_route, $routes);
+		//Create route
+		$route = array($url => "$base/$controller/getJavascript");
+		$this->getApplication()->getConfig()->webApplication->routes = array_merge($this->getApplication()->getConfig()->webApplication->routes->toArray(), $route);
 	}
 
 	/**
-	 * Method retunrs the root URL for this package.
+	 * Method returns the root URL for this package.
 	 */
 	public function getURL()
 	{
 		return CKernel::getInstance()->convertArbitrageNamespaceToURL($this->getNamespace());
+	}
+
+	/**
+	 * Method adds a route rule to the routing logic.
+	 * @param $match The URL to match.
+	 * @param $url The url to route to.
+	 * @param $opt_prepend Optional parameter indiciating if we should prepend the route.
+	 */
+	public function addRoute($match, $url, $prepend=false)
+	{
+		$routes = $this->getApplication()->getRoutes()->toArray();
+
+		//Check if we are prepending or not
+		if($prepend)
+			$routes = array_merge(array($match => $url), $routes);
+		else
+			$routes = array_merge($routes, array($match => $url));
+
+		//Set new routes
+		$this->getApplication()->getRoutes()->setData($routes);
 	}
 }
 ?>
