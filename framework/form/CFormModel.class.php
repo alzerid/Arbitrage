1d68fa6893c897d3b8e859bbe0053e68d4717111
Eric Janik <eric@dualclutchmedia.com>
2012-08-27 07:30:19 -0500

    added CFormModel

---
+++ framework/form/CFormModel.class.php
@@ -0,0 +1,32 @@
+<?
+namespace Framework\Forms;
+
+class CFormModel extends \Framework\Utils\CArrayObject
+{
+	/**
+	 * Method converts the FormModel values to a \Framework\Interface\IModel.
+	 * @param $model The arbitrage namespace of the model to convert to.
+	 */
+	public function convertToDatabaseModel($model)
+	{
+		$model = \Framework\Base\CKernel::getInstance()->convertArbitrageNamespaceToPHP($model);
+
+		//Ensure model is loaded
+		try
+		{
+			$instance = $model::load();
+		}
+		catch(\Exception $ex)
+		{
+			throw new \Framework\Exceptions\EArbitrageException("Model '$model' does not exist!");
+		}
+
+		//Ensure class inherits \Framework\Interfaces\IModel
+		if(!($instance instanceof \Framework\Interfaces\IModel))
+			throw new \Framework\Exceptions\EArbitrageException("Model '$model' does not inherit \\Framework\\Interfaces\\IModel.");
+
+		//Convert to model specified in the parameter
+		return $model::model($this->_data);
+	}
+}
+?>
