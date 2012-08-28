9a9ade688d81e5d66a3c3359edd2d26e7f7a2b4f
Eric Janik <eric@dualclutchmedia.com>
2012-08-27 21:17:13 -0500

    removed methods. converted CDataTable to an abstract

--- framework/html/CDataTable.class.php
+++ framework/html/CDataTable.class.php
@@ -1,7 +1,7 @@
 <?
 namespace Framework\HTML;
 
-class CDataTable implements \Framework\Interfaces\IHTMLDataTable
+abstract class CDataTable implements \Framework\Interfaces\IHTMLDataTable
 {
 	protected $_headers;
 	protected $_data;
@@ -11,79 +11,19 @@ class CDataTable implements \Framework\Interfaces\IHTMLDataTable
 	public function __construct($id, $headers, $data, $attrs=array())
 	{
 		//Set variables
-		$this->_data  = $data;
-		$this->_attrs = $attrs;
-		$this->_id    = $id;
+		$this->_data    = $data;
+		$this->_attrs   = $attrs;
+		$this->_id      = $id;
+		$this->_headers = $headers;
 
-		//Normalize headers
-		$new = array();
-		foreach($headers as $key => $val)
-		{
-			if(is_string($val))
-			{
-				$val = preg_replace('/\./', '/', $val);
-				$val = "/$val";
-			}
-
-			$new[$key] = $val;
-		}
-
-		$this->_headers = $new;
+		//Add datatable class
+		if(empty($this->_attrs['class']))
+			$this->_attrs['class'] = "datatable";
+		else
+			$this->_attrs['class'] = ' datatable';
 	}
 
-	public function render()
-	{
-		$attrs = CHTMLComponent::generateAttribs($this->_attrs);
-		$html  = "<table id=\"{$this->_id}\" $attrs>";
-
-		$html .= $this->_renderHeader();
-		$html .= $this->_renderData();
-
-		$html .= "</table>";
-
-		return $html;
-	}
-	
-	protected function _renderHeader()
-	{
-		$html = "<thead><tr>";
-		foreach($this->_headers as $title => $val)
-			$html .= "<th>$title</th>";
-
-		$html .= "</tr></thead>";
-
-		return $html;
-	}
-
-	protected function _renderData()
-	{
-		$html = "<tbody>";
-
-		foreach($this->_data as $entry)
-		{
-			$html .= "<tr>";
-
-			foreach($this->_headers as $key => $val)
-			{
-				//Check what val type
-				if(gettype($val) === "string")
-				{
-					$arr = new CArrayObject($entry);
-					$val = $this->_normalizeValue($arr->xpath($val));
-				}
-				elseif($val instanceof \Framework\Interfaces\IHTMLDataTableEntry)
-					$val = $val->render($this, $entry);
-
-				$html .= "<td>$val</td>";
-			}
-
-			$html .= "</tr>";
-		}
-
-		$html .= "</tbody>";
-
-		return $html;
-	}
+	abstract public function render();
 
 	protected function _normalizeValue($val)
 	{
