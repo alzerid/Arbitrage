7a1517a6675aae39641a5995af064896384d1db9
Eric Janik <eric@dualclutchmedia.com>
2012-08-27 21:19:16 -0500

    updated CModelData to implement IArbitragePath

--- framework/database/types/CModelData.class.php
+++ framework/database/types/CModelData.class.php
@@ -1,7 +1,7 @@
 <?
 namespace Framework\Database\Types;
 
-class CModelData implements \ArrayAccess
+class CModelData implements \ArrayAccess, \Framework\Interfaces\IArbitragePath
 {
 	static private $_TYPES    = array();  //Typecasts
 	static private $_DEFAULTS = array();  //Cached defaults
@@ -107,6 +107,18 @@ class CModelData implements \ArrayAccess
 		}
 
 		return $ret;
+	}
+
+	/**
+	 * Method traverses the values with Arbitrage Namespace Notation.
+	 * @param string $path The arbitrage path.
+	 * @return mixed Returns a value or a \Framework\Utils\CArrayObject
+	 */
+	public function apath($path)
+	{
+		//Get value from _variables
+		$val = $this->_apath($path, $this->_variables);
+		return (($val === NULL)? $this->_apath($path, $this->_originals) : $val);
 	}
 
 	protected function _setModelData(array &$originals=array())
@@ -223,6 +235,28 @@ class CModelData implements \ArrayAccess
 	{
 		if(array_key_exists($name, $this->_variables))
 			unset($this->_variables[$name]);
+	}
+
+	/**
+	 * Method traverses the values with Arbitrage Namespace Notation.
+	 * @param string $path The arbitrage path.
+	 * @param array $arr The array to traverse.
+	 * @return mixed Returns a value or a \Framework\Utils\CArrayObject
+	 */
+	private function _apath($path, array $arr)
+	{
+		//Get value
+		$path = explode('.', $path);
+		$key  = array_splice($path, 0, 1);
+		$path = implode('.', $path);
+		$key  = $key[0];
+		$val  = ((!empty($arr[$key]))? $arr[$key] : NULL);
+
+		//Recursive
+		if(is_array($val))
+			return $this->_apath($path, $val);
+
+		return $val;
 	}
 }
 ?>
