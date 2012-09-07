f58b27062bf5268e2cf0305554a82e73b1a827b2
Eric Janik <eric@dualclutchmedia.com>
2012-09-07 15:00:43 -0500

    added setValue, getValue, and instantiate method

--- framework/database/data_types/CDatabaseIDDataType.class.php
+++ framework/database/data_types/CDatabaseIDDataType.class.php
@@ -1,8 +1,46 @@
 <?
 namespace Framework\Database\DataTypes;
 
-class CDatabaseIDDataType extends \Framework\Model\CModel
+class CDatabaseIDDataType implements \Framework\Interfaces\IModelDataType
 {
+	protected $_id;
 
+	public function __construct()
+	{
+		$this->_id = NULL;
+	}
+
+	/**
+	 * Instantiate method creates a new ID
+	 * @param $id The id to instantiate.
+	 * @return \Framework\Database\DataTypes\CDatabaseIDDataType
+	 */
+	static public function instantiate($id)
+	{
+		$class    = get_called_class();
+		$obj      = new $class;
+
+		$obj->setValue($id);
+
+		return $obj;
+	}
+
+	/**
+	 * Method sets the ID.
+	 * @param $id The id to set to.
+	 */
+	public function setValue($id=-1)
+	{
+		$this->_id = $id;
+	}
+
+	/**
+	 * Method returns the ID.
+	 * @return Returns the id.
+	 */
+	public function getValue()
+	{
+		return $this->_id;
+	}
 }
 ?>
