a499c60b5d59ffe027ca9c59c56c0af327b8ca0f
Eric Janik <eric@dualclutchmedia.com>
2012-09-07 15:01:06 -0500

    updated to use internal methods

--- framework/database/structures/CArrayStructure.class.php
+++ framework/database/structures/CArrayStructure.class.php
@@ -5,6 +5,12 @@ class CArrayStructure extends \Framework\Model\Structures\CArrayStructure implem
 {
 	protected $_originals;
 
+	public function __construct()
+	{
+		parent::__construct();
+		$this->_originals = array();
+	}
+
 	/**
 	 * Method instantiates the data type.
 	 * @param $data The variables to set as default data for this Model.
@@ -13,13 +19,23 @@ class CArrayStructure extends \Framework\Model\Structures\CArrayStructure implem
 	static public function instantiate($data=array(), $class=NULL)
 	{
 		$obj = parent::instantiate($data, $class);
-		$obj->_originals = $obj->_data;
+
+		//Set originals
+		if($data instanceof \Framework\Database\Structures\CArrayStructure)
+			$obj->_originals = $data->_originals;
+		else
+			$obj->_originals = $obj->_data;
 
 		return $obj;
 	}
 
+	/**
+	 * Method returns the updated query.
+	 * @return array Retuns an array of the updated items.
+	 */
 	public function getUpdateQuery()
 	{
+		//TODO: Code smarter differences
 		if(count($this->_data) == 0)
 			return $this->_data;
 
@@ -27,12 +43,30 @@ class CArrayStructure extends \Framework\Model\Structures\CArrayStructure implem
 		if(count($ret) == 0)
 			return NULL;
 
-		return $ret;
+		return $this->_data;
 	}
 
+	/**
+	 * Method sets _originals to _data.
+	 */
+	public function merge()
+	{
+		$this->_originals = $this->_data;
+	}
+
+	/**
+	 * Method clears the array back to it's original contents.
+	 */
 	public function clear()
 	{
+		die(__METHOD__);
 		$this->_data = $this->_originals;
+	}
+
+	protected function _setModelData($vars)
+	{
+		parent::_setModelData($vars);
+		$this->_originals = $vars;
 	}
 }
 ?>
