5fe7b7e607050dac32f29471ea6b1df9610d1a01
Eric Janik <eric@dualclutchmedia.com>
2012-09-11 11:32:41 -0500

    added iterator capabilities. put back limit, skip attributes

--- framework/database/CDriverQuery.class.php
+++ framework/database/CDriverQuery.class.php
@@ -1,14 +1,16 @@
 <?
 namespace Framework\Database;
 
-abstract class CDriverQuery
+abstract class CDriverQuery implements \ArrayAccess, \Iterator
 {
-	protected $_class;    //Class to use when creating Models
-	protected $_driver;   //Driver object to use for querying
+	protected $_class;        //Class to use when creating Models
+	protected $_driver;       //Driver object to use for querying
+	protected $_collection;   //Collection set
 
 	//Query
 	protected $_query;
 	protected $_data;
+	protected $_sort;
 	protected $_limit;
 	protected $_skip;
 	protected $_cmd;
@@ -21,8 +23,14 @@ abstract class CDriverQuery
 	public function __construct(\Framework\Database\CDatabaseDriver $driver, $class)
 	{
 		//Database
-		$this->_class  = $class;
-		$this->_driver = $driver;
+		$this->_class      = $class;
+		$this->_driver     = $driver;
+		$this->_collection = NULL;
+
+		//Query Modifiers
+		$this->_sort  = NULL;
+		$this->_limit = NULL;
+		$this->_skip  = NULL;
 
 		//Query
 		$this->_query = NULL;
@@ -32,20 +40,18 @@ abstract class CDriverQuery
 
 	abstract public function findOne($query);
 	abstract public function findAll($query);
-	abstract public function count($query);
+	//abstract public function count($query);
 	abstract public function update($query, $data);
 	abstract public function upsert($query, $data);
 	abstract public function insert($data);
 	abstract public function save($data);
 	abstract public function remove($query);
 
-	//Actually execute
 	/**
 	 * Abstract method that must be defined in the driver.
-	 * @param \Framework\Database\CDatabaseModelCollection $results The result object to use for querying and setting the results.
 	 * @return \Framework\Database\CDatabaseModelCollection Retuns the result.
 	 */
-	abstract public function execute(\Framework\Database\CDatabaseModelCollection $results);
+	abstract public function execute();
 
 	/**
 	 * Method returns the model class associated with this query object.
@@ -64,5 +70,94 @@ abstract class CDriverQuery
 	{
 		return $this->_driver;
 	}
+
+	/**
+	 * Method returns the collection associated with the query.
+	 * @return \Framework\Database\CDatabaseModelCollection Returns the collection.
+	 */
+	public function getCollection()
+	{
+		return $this->_collection;
+	}
+
+	/** Query Property Modifiers **/
+
+	/**
+	 * Methods sets a sort on the query.
+	 * @param $sort The sorting property to set when querying.
+	 * @returns \Framework\Database\CDatabaseModelCollection Returns itself.
+	 */
+	public function sort($sort)
+	{
+		$this->_sort = $sort;
+		return $this;
+	}
+
+
+	/**
+	 * Methods sets a limit on the query.
+	 * @param $limit The limit property to set when querying.
+	 * @returns \Framework\Database\CDatabaseModelCollection Returns itself.
+	 */
+	public function limit($limit)
+	{
+		$this->_limit = $limit;
+		return $this;
+	}
+
+	/**
+	 * Methods sets a skip on the query.
+	 * @param $skip The skip property to set when querying.
+	 * @returns \Framework\Database\CDatabaseModelCollection Returns itself.
+	 */
+	public function skip($skip)
+	{
+		$this->_skip = $skip;
+		return $this;
+	}
+	/** END Query Property Modifiers **/
+
+	/*****************************/
+	/** Iterator Implementation **/
+	/*****************************/
+
+	public function count()
+	{
+		return $this->_getCollection()->count();
+	}
+
+	public function current()
+	{
+		return $this->_getCollection()->current();
+	}
+
+	public function key()
+	{
+		return $this->_getCollection()->key();
+	}
+
+	public function next()
+	{
+		return $this->_getCollection()->next();
+	}
+
+	public function rewind()
+	{
+		return $this->_getCollection()->rewind();
+	}
+
+	public function valid()
+	{
+		return $this->_getCollection()->valid();
+	}
+
+	/*********************************/
+	/** END Iterator Implementation **/
+	/*********************************/
+
+	/**
+	 * Method gets the collection and assigns it locally.
+	 */
+	abstract protected function _getCollection();
 }
 ?>
