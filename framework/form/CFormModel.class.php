f9f5ac8abe56f5257bc61c60e9effe8295e82b4f
Eric Janik <eric@dualclutchmedia.com>
2012-09-20 12:25:33 -0500

    added convertToModel and toArray methods

--- framework/form/CFormModel.class.php
+++ framework/form/CFormModel.class.php
@@ -17,6 +17,56 @@ class CFormModel extends \Framework\Model\CModel
 	}
 
 	/**
+	 * Method converts the model to a database model.
+	 * @param $namespace The namespace model.
+	 * @param @return \Framework\Database\CModel The database model or NULl.
+	 */
+	public function convertToModel($namespace)
+	{
+		$class = \Framework\Base\CKernel::getInstance()->convertArbitrageNamespaceToPHP($namespace);
+		if(!class_exists($class))
+			throw new \Framework\Exceptions\EModelException("Database model '$class' does not exist!");
+
+		//Create object
+		$obj = $class::instantiate();
+		foreach($this->_data as $key=>$val)
+		{
+			if(isset($obj->$key))
+			{
+				if($val instanceof \Framework\Form\Elements\CBaseFormElement)
+					$val = $val->getValue();
+
+				$obj->$key = $val;
+			}
+		}
+
+		return $obj;
+	}
+
+	/**
+	 * Method returns the array key value pairs.
+	 */
+	public function toArray()
+	{
+		//TODO: Code array value
+
+		$ret = array();
+		foreach($this->_data as $key=>$data)
+		{
+			if($data instanceof \Framework\Form\Elements\CBaseFormElement)
+				$ret[$key] = $data->getValue();
+			elseif($data instanceof \Framework\Model\CModel)
+			{
+				die(__METHOD__);
+			}
+			else
+				$ret[$key] = $data;
+		}
+
+		return $ret;
+	}
+
+	/**
 	 * Method sets the data.
 	 * @param $name The name of the attribute to set.
 	 * @param $val The value to set the attribute to.
@@ -48,32 +98,5 @@ class CFormModel extends \Framework\Model\CModel
 
 		return parent::_getData($name);
 	}
-
-	/**
-	 * Method converts the FormModel values to a \Framework\Interface\IModel.
-	 * @param $model The arbitrage namespace of the model to convert to.
-	 */
-	/*public function convertToDatabaseModel($model)
-	{
-		$model = \Framework\Base\CKernel::getInstance()->convertArbitrageNamespaceToPHP($model);
-
-		//Ensure model is loaded
-		try
-		{
-			$instance = $model::load();
-		}
-		catch(\Exception $ex)
-		{
-			throw new \Framework\Exceptions\EArbitrageException("Model '$model' does not exist!");
-		}
-
-		//Ensure class inherits \Framework\Interfaces\IModel
-		if(!($instance instanceof \Framework\Interfaces\IModel))
-			throw new \Framework\Exceptions\EArbitrageException("Model '$model' does not inherit \\Framework\\Interfaces\\IModel.");
-
-		//Convert to model specified in the parameter
-		//TODO: Code conversion
-		return $model::instantiate($this->_data);
-	}*/
 }
 ?>
