99ca92a8926962f0693f77445f4cccdc12f75211
Eric Janik <eric@dualclutchmedia.com>
2012-12-07 18:42:00 -0600

    added data types

---
+++ framework/database2/model/data_types/CDataType.class.php
@@ -0,0 +1,29 @@
+<?php
+namespace Framework\Database2\Model\DataTypes;
+
+abstract class CDataType
+{
+	protected $_val;
+
+	public function __construct($val=NULL)
+	{
+		$this->set($val);
+	}
+
+	public function __toString()
+	{
+		$this->_toString();
+	}
+
+	/**
+	 * Method called when we want to set a value to the DataType.
+	 */
+	abstract public function set($val);
+
+	/**
+	 * Abstract method converts the data type to a string.
+	 * @return string Returns a string.
+	 */
+	abstract protected function _toString();
+}
+?>
---
+++ framework/database2/model/data_types/CDatabaseID.class.php
@@ -0,0 +1,24 @@
+<?php
+namespace Framework\Database2\Model\DataTypes;
+
+class CDatabaseID extends \Framework\Database2\Model\DataTypes\CDataType
+{
+	/**
+	 * Method called to set the the value of this DataType.
+	 * @param $val The value to set to.
+	 */
+	public function set($val)
+	{
+		$this->_val = $val;
+	}
+
+	/**
+	 * Abstract method converts the data type to a string.
+	 * @return string Returns a string.
+	 */
+	protected function _toString()
+	{
+		die(__METHOD__);
+	}
+}
+?>
